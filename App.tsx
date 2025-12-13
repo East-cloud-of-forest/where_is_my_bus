e40891862b984b207ceca392ae084602e8c85e52
ecof <ecof1911@gmail.com>
2025-12-14 00:46:52 +0900

    feat: setup react native project with docker and env config

--- App.tsx
+++ App.tsx
@@ -4,42 +4,64 @@
  *
  * @format
  */
+// 앱의 메인 진입점 파일입니다.
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-function App() {
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1, // 전체 화면을 사용하도록 설정
+    justifyContent: 'center' as 'center', // 세로 중앙 정렬
+    alignItems: 'center' as 'center', // 가로 중앙 정렬
+  };
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
       />
-    </View>
+      <View>
+        <Text style={[styles.text, {color: isDarkMode ? Colors.white : Colors.black}]}>
+          내 버스 언제 와?
+        </Text>
+        <Text style={[styles.subText, {color: isDarkMode ? Colors.light : Colors.dark}]}>
+             React Native 프로젝트가 설치되었습니다.
+        </Text>
+        <Text style={[styles.subText, {color: isDarkMode ? Colors.light : Colors.dark}]}>
+             (D:\creative\develop\where_is_my_bus)
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
   },
+  subText: {
+    fontSize: 16,
+    textAlign: 'center',
+  }
 });
 
 export default App;
